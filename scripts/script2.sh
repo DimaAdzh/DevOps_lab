2fab90e14f4172b7f33ec00e2f7b74a28bd071d3
DimaAdzh <dimaadzubej49@gmail.com>
2024-01-04 08:04:57 +0200

    add-scripts

---
+++ scripts/script2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "Використання: $0 <шлях до директорії> <текст для пошуку>"
+  exit 1
+fi
+
+directory="$1"
+search_text="$2"
+
+if [ ! -d "$directory" ]; then
+  echo "Директорія не існує: $directory"
+  exit 1
+fi
+
+grep -rl "$search_text" "$directory"
